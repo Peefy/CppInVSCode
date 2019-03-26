b08c7c26fc1b3d95e007dd95b97c3cde26acd559
Peefy <xpf6677@163.com>
2019-03-26 21:52:36 +0800

    update

--- src/smartpointer.cpp
+++ src/smartpointer.cpp
@@ -7,21 +7,56 @@ namespace DuGu
 {
 static void sharedptrDemo(){
     //auto ptr = std::shared_ptr<int>(1);
-    printf("hello");
+    printf("hello shared_ptr\n");
+    int a = 10;
+    std::shared_ptr<int> ptra = std::make_shared<int>(a);
+    std::shared_ptr<int> ptra2(ptra);
+    printf("the use count of ptra is %d\n", ptra.use_count());
+    int b = 20;
+    int *pb = &a;
+    //std::shared_ptr<int> ptrb = pb;  //error
+    std::shared_ptr<int> ptrb = std::make_shared<int>(b);
+    printf("the use count of ptrb is %d\n", ptrb.use_count());
+    ptra = ptrb;
+    pb = ptrb.get();
+    printf("the use count of ptra is %d\n", ptra.use_count());
+    printf("the use count of ptrb is %d\n", ptrb.use_count());
 }
 
 static void uniqueptrDemo(){
-    
+    std::unique_ptr<int> uptr(new int(10));
+    std::unique_ptr<int> uptr2 = std::move(uptr);
+    uptr2.release();
 }
 
 static void weakptrDemo(){
-    
+    std::shared_ptr<int> sh_ptr = std::make_shared<int>(10);
+    printf("the use count of shared ptr is %d\n", sh_ptr.use_count());
+
+    std::weak_ptr<int> wp(sh_ptr);
+    printf("the use count of weak ptr is %d\n", wp.use_count());
+
+    if(!wp.expired()){
+        std::shared_ptr<int> sh_ptr2 = wp.lock(); 
+        *sh_ptr = 100;
+        printf("the use count of weak ptr is %d\n", wp.use_count());
+    }
+}
+
+static mySmartPointerDemo(){
+    Smartpointer<TempClass> sp1(new TempClass(1));
+    Smartpointer<TempClass> sp2(sp1);
+    Smartpointer<TempClass> sp3(new TempClass(2));
+    sp2 = sp3;
+    printf("the use count of sp1 ptr is %d\n", sp1.use_count());
+    printf("the use count of sp3 ptr is %d\n", sp3.use_count());
 }
 
 void smartPointerDemo(){
     sharedptrDemo();
     uniqueptrDemo();
     weakptrDemo();
+    mySmartPointerDemo();
 }
 }
 
--- src/smartpointer.h
+++ src/smartpointer.h
@@ -3,23 +3,79 @@
 #ifndef _SMART_PONTER_H_
 #define _SMART_PONTER_H_
 
+#include <iostream>
+#include <memory>
+
 namespace DuGu
 {
+template<typename T>
 class Smartpointer
 {
   private:
-    /* data */
+    T* _ptr;
+    size_t* _count;
+    void releaseCount(){
+      if (this->_ptr){
+        (*this->_count)--;
+        if (this->_count == 0){
+          delete this->_ptr;
+          delete this->_count;
+        }
+      }
+    }
   public:
-    Smartpointer(/* args */) {}
-    ~Smartpointer() {}
+    //cotr
+    Smartpointer(T* ptr = nullptr) : _ptr(ptr){
+      _count = _ptr ? new size_t(1) : new size_t(0);
+    }
+    //copy_cotr
+    Smartpointer(const Smartpointer& ptr){
+      if (this != &ptr){
+        this->_ptr = ptr._ptr;
+        this->_count = ptr._count;
+        (*this->_count)++;
+      }
+    }
+    //=
+    Smartpointer& operator=(const Smartpointer&ptr){
+      if (this->_ptr == ptr._ptr){
+        return *this;
+      }
+      this->releaseCount();
+      this->_ptr = ptr._ptr;
+      this->_count = ptr._count;
+      (*this->_count)++;
+      return *this;
+    }
+
+    T& operator*(){
+      assert(this->_ptr == nullptr);
+      return *(this->_ptr);
+    }
+
+    T* operator->(){
+      assert(this->_ptr == nullptr);
+      return this->_ptr;
+    }
+
+    ~Smartpointer() {
+      if (--(*this->_count) == 0){
+          delete this->_ptr;
+          delete this->_count;
+      }
+    }
+
+    size_t use_count() const{
+      return *this->_count;
+    }
 };
 
 class TempClass
 {
 private:
-  /* data */
+  int _index;
 public:
-  TempClass(/* args */) { }
+  TempClass(int index) : _index(index) { }
   ~TempClass() { }
   void Print(){}
 };

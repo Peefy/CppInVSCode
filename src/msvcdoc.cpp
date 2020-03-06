c87668c841697820bf777a64167ee5dc699491be
Peefy <xpf6677@163.com>
2020-03-06 15:43:59 +0800

    Finish atomic header

--- src/msvcdoc.cpp
+++ src/msvcdoc.cpp
@@ -562,7 +562,30 @@ void header_atomic() {
 	 * atomic_thread_fence	充当就其他 fence 建立内存排序要求的 fence。
 	 * kill_dependency	中断可能的依赖关系链。
 	*/
+	std::atomic<bool> ready(false);    // can be checked without being set
+	std::atomic_flag winner = ATOMIC_FLAG_INIT;    // always set when checked
+	auto count1m = [](int id) -> void
+	{
+    	while (!ready) {
+        	std::this_thread::yield();
+    	} // 等待主线程中设置 ready 为 true.
+    	for (int i = 0; i < 1000000; ++i) {
+    	} // 计数.
 
+    	// 如果某个线程率先执行完上面的计数过程，则输出自己的 ID.
+    	// 此后其他线程执行 test_and_set 是 if 语句判断为 false，
+    	// 因此不会输出自身 ID.
+    	if (!winner.test_and_set()) {
+        	std::cout << "thread #" << id << " won!\n";
+    	}
+	};
+	std::vector<std::thread> threads;
+    std::cout << "spawning 10 threads that count to 1 million...\n";
+    for (int i = 1; i <= 10; ++i)
+        threads.push_back(std::thread(count1m, i));
+    ready = true;
+    for (auto & th:threads)
+        th.join();
 }
 
 int msvc_main() {

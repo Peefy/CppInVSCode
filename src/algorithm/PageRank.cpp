c3b9dbc3dc5c12b7f844476465d1420c97db0f9b
Peefy <xpf6677@163.com>
2020-04-09 22:01:19 +0800

    Add cpp algorithm files

---
+++ src/algorithm/PageRank.cpp
@@ -0,0 +1,188 @@
+
+#include "stdafx.h"
+#include "stdio.h"
+
+#include <cstdio>   
+#include <cmath>
+#include <cstring>
+#include <iostream>  
+#include <vector>
+#include <stack>
+#include <cstring>
+#include <map>
+
+using namespace std;  
+
+#define FOR_RANGE(x1, x2) for(int i = x1; i < x2; i++)  
+#define TOMIN(x1, x2) x1 = x1 < x2 ? x1 : x2;  
+#define N 1001  
+#define M 10001 
+
+struct edge
+{ 
+	int u, v;
+} a[M];  
+int from[N]; 
+int to[M];  
+int n, m;  
+int dfn[N], low[N];  
+int num;  
+stack<int> s; int ins[N];  
+
+map<int, int> maxscc;
+map<int, int> scc;
+int k = 0;
+int anwserOrder[N] = {-1};
+double Mv[N][N] = {{0}};
+double v[M] = {0};
+double vout[M] = {0};
+const double eps = 0.00000001;
+
+void addedge(int u, int v, int e) 
+{
+	to[e] = from[u]; 
+	from[u] = e; 
+	a[e].u = u; 
+	a[e].v = v;
+}  
+
+void tarjin(int u)  
+{  
+	dfn[u] = low[u] = ++num;  
+	s.push(u);  
+	ins[u] = 1;  
+	for(int e = from[u]; e != -1; e = to[e])  
+	{  
+		int  v= a[e].v;  
+		if(!dfn[v])  
+		{  
+			tarjin(v);  
+			TOMIN(low[u], low[v]);  
+		}  
+		else if(ins[v])  
+		{  
+			TOMIN(low[u], dfn[v]);  
+		}  
+	}  
+	if (dfn[u] == low[u])  
+	{  
+		int scccount = 0;
+		while(1)  
+		{  
+			int v = s.top();  
+			s.pop();  
+			ins[v] = 0;  
+			scc[v] = scccount++;
+			if(u == v) 
+				break;  
+		}  
+		if (scc.size() > 1)
+		{
+			maxscc = scc;
+		}
+		scc.clear();
+	}  
+}  
+
+void findmaxscc()
+{
+	FOR_RANGE(1, n + 1)  
+	{  
+		if(!dfn[i])  
+			tarjin(i);  
+	} 
+}
+
+inline bool inscc(int v)
+{
+	return maxscc.find(v) != maxscc.end();
+}
+
+double norm(double* v1, double* v2, int vm)
+{
+	double sum = 0;
+	for (int i = 0;i < vm;++i)
+	{
+		sum += (v1[i] - v2[i]) * (v1[i] - v2[i]);
+	}
+	return sqrt(sum);
+}
+
+bool matrixMultiplyVector(double matrix1[N][N], double vector[M], double(&result)[M], int vm) {
+	for (int k = 0; k < vm; k++) 
+	{
+		result[k] = 0.0;
+		for (int j = 0; j < vm; j++) 
+		{
+			result[k] = result[k] + matrix1[k][j] * vector[j];
+		}
+	}
+	return true;
+}
+
+
+void pageRank()
+{
+	int sccCount = maxscc.size();
+	double factor = 1.0 / (double)sccCount;
+	for (int i = 0;i < sccCount;++i)
+	{
+		v[i] = factor;
+	}
+	for (int i = 0;i < m;++i)
+	{
+		if (inscc(a[i].u) && inscc(a[i].v))
+		{
+			Mv[maxscc[a[i].v]][maxscc[a[i].u]] = 1;
+		}
+	}	
+	for (int i = 0;i < sccCount;++i)
+	{
+		double sum = 0;
+		for (int j = 0;j < sccCount;++j)
+		{
+			sum += Mv[j][i];
+		}
+		for (int j = 0;j < sccCount;++j)
+		{
+			Mv[j][i] /= sum;
+		}
+	}
+	while (true)
+	{
+		matrixMultiplyVector(Mv, v, vout, sccCount);
+		if (norm(v, vout, sccCount) > eps)
+			memcpy(v, vout, sizeof(double) * sccCount);
+		else
+			break;
+	}
+}
+
+void generateInput() 
+{
+	num = 0;  
+	memset(dfn, 0, sizeof(dfn));  
+	memset(from, -1, sizeof(from));  
+	memset(ins, 0, sizeof(ins)); 
+	scanf("%d", &n);scanf("%d", &m);
+	FOR_RANGE(0, m){int u,v; scanf("%d", &u);scanf("%d", &v);; addedge(u, v, i);} 
+	cin>>k;
+	FOR_RANGE(0, k){scanf("%d", &anwserOrder[i]);}
+}  
+
+void generateOutput()
+{
+	for (int i = 0;i < k;++i)
+	{
+		inscc(anwserOrder[i]) == false ? printf("None\r\n") : printf("%.5f\r\n", vout[maxscc[anwserOrder[i]]]);
+	}
+}
+
+int main()  
+{  
+	generateInput();  
+	findmaxscc();
+	pageRank();
+	generateOutput();
+	return 0;
+}  
---
+++ src/algorithm/shortestPath1.cpp
@@ -0,0 +1,197 @@
+#include <stdio.h>
+#include <math.h>
+#include <memory.h>
+
+#define N 101
+int W[N][N] = {{0}};
+int L[N][N] = {{0}};
+int Llast[N][N] = {{0}};
+
+void generateInput(int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            scanf("%d", &W[i][j]);
+        }
+    }
+}
+
+void extendShotestPath(int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            L[i][j] = -1;
+            for (int k = 0; k < n; ++k)
+            {
+                if (Llast[i][k] == -1 || W[k][j] == -1)
+                    L[i][j] = L[i][j];
+                else if (L[i][j] == -1)
+                    L[i][j] = Llast[i][k] + W[k][j];
+                else
+                    L[i][j] = L[i][j] < Llast[i][k] + W[k][j] ? L[i][j] : Llast[i][k] + W[k][j];
+            }
+        }
+    }
+}
+
+void allPairsShortestPath(int n)
+{
+    memcpy(Llast, W, sizeof(int) * N * N);
+    for (int m = 1; m < n - 1; ++m)
+    {
+        extendShotestPath(n);
+        memcpy(Llast, L, sizeof(int) * N * N);
+    }
+}
+
+void floydWarshall(int n)
+{
+    memcpy(L, W, sizeof(int) * N * N);
+    memcpy(Llast, W, sizeof(int) * N * N);
+    for (int k = 0; k < n; ++k)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (Llast[i][k] == -1 || Llast[k][j] == -1)
+                    L[i][j] = Llast[i][j];
+                else if (Llast[i][j] == -1)
+                    L[i][j] = Llast[i][k] + Llast[k][j];
+                else
+                {
+                    if (Llast[i][j] > Llast[i][k] + Llast[k][j])
+                        L[i][j] = Llast[i][k] + Llast[k][j];
+                    else
+                        L[i][j] = Llast[i][j];
+                }
+            }
+        }
+        memcpy(Llast, L, sizeof(int) * N * N);
+    }
+}
+
+void generateOutPut(int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            printf("%d ", L[i][j]);
+        }
+        printf("\r\n");
+    }
+}
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    generateInput(n);
+    //allPairsShortestPath(n);
+    floydWarshall(n);
+    generateOutPut(n);
+    return 0;
+}#include <stdio.h>
+#include <math.h>
+#include <memory.h>
+
+#define N 101
+int W[N][N] = {{0}};
+int L[N][N] = {{0}};
+int Llast[N][N] = {{0}};
+
+void generateInput(int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            scanf("%d", &W[i][j]);
+        }
+    }
+}
+
+void extendShotestPath(int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            L[i][j] = -1;
+            for (int k = 0; k < n; ++k)
+            {
+                if (Llast[i][k] == -1 || W[k][j] == -1)
+                    L[i][j] = L[i][j];
+                else if (L[i][j] == -1)
+                    L[i][j] = Llast[i][k] + W[k][j];
+                else
+                    L[i][j] = L[i][j] < Llast[i][k] + W[k][j] ? L[i][j] : Llast[i][k] + W[k][j];
+            }
+        }
+    }
+}
+
+void allPairsShortestPath(int n)
+{
+    memcpy(Llast, W, sizeof(int) * N * N);
+    for (int m = 1; m < n - 1; ++m)
+    {
+        extendShotestPath(n);
+        memcpy(Llast, L, sizeof(int) * N * N);
+    }
+}
+
+void floydWarshall(int n)
+{
+    memcpy(L, W, sizeof(int) * N * N);
+    memcpy(Llast, W, sizeof(int) * N * N);
+    for (int k = 0; k < n; ++k)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (Llast[i][k] == -1 || Llast[k][j] == -1)
+                    L[i][j] = Llast[i][j];
+                else if (Llast[i][j] == -1)
+                    L[i][j] = Llast[i][k] + Llast[k][j];
+                else
+                {
+                    if (Llast[i][j] > Llast[i][k] + Llast[k][j])
+                        L[i][j] = Llast[i][k] + Llast[k][j];
+                    else
+                        L[i][j] = Llast[i][j];
+                }
+            }
+        }
+        memcpy(Llast, L, sizeof(int) * N * N);
+    }
+}
+
+void generateOutPut(int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            printf("%d ", L[i][j]);
+        }
+        printf("\r\n");
+    }
+}
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    generateInput(n);
+    //allPairsShortestPath(n);
+    floydWarshall(n);
+    generateOutPut(n);
+    return 0;
+}
